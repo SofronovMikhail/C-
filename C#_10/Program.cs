d4983e5a3c0201ada08a53e0aae8109288fae076
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-30 14:29:45 +0300

    2 lection 3 qustion

---
+++ C#_10/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] array = {16, 34, 4, 12, 56, 3, 55, 98, 12};
+int n = array.Length;
+int find = 12;
+int i = 0;
+while(i < n){
+    if(array[i] == find){
+        Console.WriteLine(i); 
+        break;
+    }
+    i++;
+}
+
