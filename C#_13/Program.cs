e21b086678dfd4e08d297ac9ac4cb9e9e0754755
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-05 00:07:38 +0300

    Задача по сортеровке массива

---
+++ C#_13/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = {1, 5, 3, 5, 1, 7, 5, 34, 2, 56};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+            int temporary = array[i];
+            array[i] = array[minPosition];
+            array[minPosition] = temporary;
+        }
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
