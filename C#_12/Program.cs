0b339fbf1beac2e0f8aeda1288be700ccfee524b
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-04 23:42:54 +0300

    Замена старого текста на новый

---
+++ C#_12/Program.cs
@@ -0,0 +1,28 @@
+﻿string MyText = "Я думаю, сказал, князь улыбаясь-что,"
+              + "вы бы взяли приступом согласие прусского короля"
+              + "Вы так красноречивы. Вы дадите мне чаю?";
+string MyReplay(string MyText, string oldText, string newText)
+{
+    string result = string.Empty;
+    int length = MyText.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(MyText[i] == oldText)
+        {
+            result = result + $"{newText}";
+        }
+        else
+        {
+            result = result + $"{MyText[i]}";
+        }
+    }
+    return result;
+}
+string AAA = MyReplay(MyText, " ", "|");
+Console.WriteLine(AAA);
+Console.WriteLine();
+AAA = MyReplay(AAA, "к", "К");
+Console.WriteLine(AAA);
+Console.WriteLine();
+AAA = MyReplay(AAA, "С", "с");
+Console.WriteLine(AAA);
