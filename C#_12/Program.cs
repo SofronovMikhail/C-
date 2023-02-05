919b84162f525588e1ed5d57ce83db1e3077a72e
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-05 19:53:28 +0300

    изменил 12 задачу

--- C#_12/Program.cs
+++ C#_12/Program.cs
@@ -1,28 +1,28 @@
 ﻿string MyText = "Я думаю, сказал, князь улыбаясь-что,"
               + "вы бы взяли приступом согласие прусского короля"
               + "Вы так красноречивы. Вы дадите мне чаю?";
-string MyReplay(string MyText, string oldText, string newText)
+string MyReplay(string MyText, char oldText, char newText)
 {
-    string result = string.Empty;
+    string result = String.Empty;
     int length = MyText.Length;
     for(int i = 0; i < length; i++)
     {
-        if(MyText[i] == oldText)
+        if(MyText[i] == oldText) 
         {
             result = result + $"{newText}";
         }
-        else
-        {
-            result = result + $"{MyText[i]}";
-        }
+        else 
+    {
+        result = result + $"{MyText[i]}";
+    }
     }
     return result;
 }
-string AAA = MyReplay(MyText, " ", "|");
+string AAA = MyReplay(MyText, ' ', '|');
 Console.WriteLine(AAA);
 Console.WriteLine();
-AAA = MyReplay(AAA, "к", "К");
+AAA = MyReplay(AAA, 'к', 'К');
 Console.WriteLine(AAA);
 Console.WriteLine();
-AAA = MyReplay(AAA, "С", "с");
+AAA = MyReplay(AAA, 'С', 'с');
 Console.WriteLine(AAA);
